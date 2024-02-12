06bc6f566a75391cdef3b2838bc433d145c80bc1
olcayhan <kolcayhan1@gmail.com>
2024-02-12 17:26:13 +0300

    Refactor BasketItem component styling and imports

--- components/basket-item/BasketItem.tsx
+++ components/basket-item/BasketItem.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import React, { useState } from "react";
-import styles from "./BasketItem.module.scss";
 import Image from "next/image";
 import trash from "@assets/svg/trash.svg";
+import { Box, Button, Card, Flex, Input, Text } from "@chakra-ui/react";
 
 export default function BasketItem({ product }: any) {
   const [count, setCount] = useState(1);
@@ -14,30 +14,73 @@ export default function BasketItem({ product }: any) {
   const changeItem = () => {};
 
   return (
-    <div className={styles.basket_item}>
-      <div className={styles.item_left}>
-        <Image src={product.image} alt="" width={150} height={150} />
-        <p>
-          <span>{product.producerTitle}</span> {product.title}
-        </p>
-      </div>
+    <Card
+      w={"100%"}
+      display={"flex"}
+      direction={"row"}
+      justifyContent={"space-between"}
+      alignItems={"center"}
+      p={"15px"}
+    >
+      <Flex justifyContent={"flex-start"} alignItems={"center"} gap={"10px"}>
+        <Box w={"150px"} h={"150px"} position={"relative"}>
+          <Image src={product.image} alt="product-image" fill />
+        </Box>
+        <Text fontSize={"13px"} fontWeight={500} maxW={"500px"}>
+          <Text as={"span"} fontWeight={700} px={3}>
+            {product.producerTitle}
+          </Text>
+          {product.title}
+        </Text>
+      </Flex>
 
-      <div className={styles.item_right}>
-        <div className={styles.item_counter}>
-          <button onClick={incrementItem}>-</button>
-          <input
+      <Flex justifyContent={"flex-end"} alignItems={"center"} gap={"25px"}>
+        <Flex
+          justifyContent={"center"}
+          alignItems={"center"}
+          w={"150px"}
+          h={"50px"}
+          gap={"5px"}
+        >
+          <Button
+            onClick={incrementItem}
+            w={"50px"}
+            borderRadius={"50%"}
+            bgColor={"orange"}
+            color={"white"}
+          >
+            -
+          </Button>
+          <Input
             type="text"
             value={count}
             pattern="[0-9]*"
             onChange={changeItem}
+            textAlign={"center"}
           />
-          <button onClick={decrementItem}>+</button>
-        </div>
-        <p>{product.price} TL</p>
-        <button onClick={deleteItem}>
+          <Button
+            onClick={decrementItem}
+            borderRadius={"50%"}
+            w={"50px"}
+            bgColor={"orange"}
+            color={"white"}
+          >
+            +
+          </Button>
+        </Flex>
+        <Text fontSize={"18px"} fontWeight={700}>
+          {product.price} TL
+        </Text>
+        <Button
+          onClick={deleteItem}
+          w={12}
+          h={12}
+          borderRadius={"50%"}
+          bgColor={"orange.100"}
+        >
           <Image src={trash} alt="trash" width={20} height={20} />
-        </button>
-      </div>
-    </div>
+        </Button>
+      </Flex>
+    </Card>
   );
 }
