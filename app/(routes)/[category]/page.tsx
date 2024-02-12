80b19ba4ab1436a3b1f3282b3cfb930e35948b80
olcayhan <kolcayhan1@gmail.com>
2024-02-12 13:09:18 +0300

    Remove unused styles and update layout in category page

--- app/(routes)/[category]/page.tsx
+++ app/(routes)/[category]/page.tsx
@@ -1,24 +1,30 @@
 "use client";
 
 import React from "react";
-import styles from "./categories.module.scss";
 import CategoryItem from "@/components/category/CategoryItem";
 import category from "@/data/category";
 import { useParams } from "next/navigation";
+import { Flex } from "@chakra-ui/react";
 
 export default function page() {
   const params = useParams();
-
 
   let data = category.filter((item) =>
     item.category.find((item2) => item2 === params.category)
   );
 
   return (
-    <div className={styles.category_main}>
+    <Flex
+      direction={"row"}
+      alignItems={"center"}
+      justifyContent={"center"}
+      flexWrap={"wrap"}
+      gap={"50px"}
+      padding={"100px"}
+    >
       {data.map((item, key) => {
         return <CategoryItem item={item} key={key} />;
       })}
-    </div>
+    </Flex>
   );
 }
