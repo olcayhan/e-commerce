f2c0ee7ecf76cc9445b1bbd9c8bf0b421bd7c324
olcayhan <kolcayhan1@gmail.com>
2024-02-12 17:35:44 +0300

    Refactor CommentItem component to use Chakra UI components

--- components/details/comment/CommentItem.tsx
+++ components/details/comment/CommentItem.tsx
@@ -1,17 +1,40 @@
 import React from "react";
-import styles from "./commentItem.module.scss";
+import { Avatar, Card, Flex, Icon, Text } from "@chakra-ui/react";
+import { StarIcon } from "@chakra-ui/icons";
 
 export default function CommentItem({ item }: any) {
   return (
-    <div className={styles.comment_item}>
-      <h1>{item.user}</h1>
-      <p>{item.title}</p>
-      <span>{item.comment}</span>
+    <Card
+      display={"flex"}
+      direction={"column"}
+      justifyContent={"center"}
+      alignItems={"flex-start"}
+      gap={"15px"}
+      p={"20px"}
+    >
+      <Flex justifyContent={"flex-start"} alignItems={"center"} gap={"16px"}>
+        <Avatar name={item.name} />
+        <Text fontSize={24} fontWeight={700}>
+          {item.user}
+        </Text>
+        <Text fontSize={14} fontWeight={500}>
+          {item.createdAt}
+        </Text>
+      </Flex>
+      <Text fontSize={18} fontWeight={600}>
+        {item.title}
+      </Text>
+      <Text as={"span"} fontSize={14} fontWeight={500}>
+        {item.comment}
+      </Text>
 
-      <div>
-        <span>{item.rate}</span>
-        <p>{item.createdAt}</p>
-      </div>
-    </div>
+      <Flex justifyContent={"flex-start"} alignItems={"center"} gap={"10px"}>
+        <Icon as={StarIcon} />
+        <Icon as={StarIcon} />
+        <Icon as={StarIcon} />
+        <Icon as={StarIcon} />
+        <Icon as={StarIcon} />
+      </Flex>
+    </Card>
   );
 }
