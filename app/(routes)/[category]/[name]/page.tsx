413face3b043415a372219a88bc52ed75a62de9e
olcayhan <kolcayhan1@gmail.com>
2024-02-13 21:25:18 +0300

    Remove Footer component from RootLayout

--- app/(routes)/[category]/[name]/page.tsx
+++ app/(routes)/[category]/[name]/page.tsx
@@ -16,12 +16,16 @@ import {
   AccordionItem,
   AccordionPanel,
   Box,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
   Button,
   Checkbox,
   Flex,
   Icon,
 } from "@chakra-ui/react";
-import { CloseIcon } from "@chakra-ui/icons";
+import { ChevronRightIcon, CloseIcon } from "@chakra-ui/icons";
+import NotFound from "@/app/not-found";
 
 export default function page() {
   const router = useRouter();
@@ -113,157 +117,183 @@ export default function page() {
     },
   ];
 
-  return (
-    <Flex direction={"row"} gap={6}>
-      <Box
-        position={"relative"}
-        flexShrink={0}
-        w={"300px"}
-        h={"auto"}
-        border={"1px solid #e0e0e0"}
-      >
-        <Accordion
-          position={"sticky"}
-          h={"100vh"}
-          top={"0px"}
-          overflowY={"auto"}
-          allowMultiple
-          defaultIndex={[...filters.map((element, index) => index)]}
-        >
-          {filters.map((filter, index) => {
-            return (
-              <AccordionItem key={index}>
-                <AccordionButton>
-                  <Box as="span" flex="1" textAlign="left">
-                    {filter.name}
-                  </Box>
-                  <AccordionIcon />
-                </AccordionButton>
-                <AccordionPanel pb={4}>
-                  <Flex direction={"column"} gap={3}>
-                    {filter.type === "checkbox" &&
-                      filter.values.map((value, index) => {
-                        return (
-                          <Checkbox
-                            key={index}
-                            colorScheme="orange"
-                            fontWeight={600}
-                            isChecked={searchParams
-                              .getAll(filter.queryString)
-                              .includes(value)}
-                            onChange={(e) => {
-                              e.target.checked
-                                ? router.push(
-                                    pathname +
-                                      "?" +
-                                      createQueryString(
-                                        filter.queryString,
-                                        value
-                                      )
-                                  )
-                                : router.push(
-                                    pathname +
-                                      "?" +
-                                      deleteQueryString(
-                                        filter.queryString,
-                                        value
-                                      )
-                                  );
-                            }}
-                          >
-                            {value}
-                          </Checkbox>
-                        );
-                      })}
-                  </Flex>
-                </AccordionPanel>
-              </AccordionItem>
-            );
-          })}
-        </Accordion>
-      </Box>
+  const breadcrumb = [
+    { name: "Anasayfa", href: "/" },
+    { name: params.category, href: `/${params.category}` },
+    { name: params.name, href: `/${params.category}/${params.name}` },
+  ];
 
-      <Flex direction={"column"} w={"100%"} gap={3}>
-        <Flex
-          direction={"row"}
-          h={"80px"}
-          justifyContent={"flex-start"}
-          alignItems={"center"}
-          gap={3}
+  if (products.filter((item) => item.category === params.name)) {
+    return <NotFound />;
+  }
+
+  return (
+    <Box>
+      <Breadcrumb
+        py={3}
+        spacing={"8px"}
+        separator={<ChevronRightIcon color="orange" />}
+      >
+        {breadcrumb.map((item, index) => {
+          return (
+            <BreadcrumbItem key={index}>
+              <BreadcrumbLink href={item.href}>{item.name}</BreadcrumbLink>
+            </BreadcrumbItem>
+          );
+        })}
+      </Breadcrumb>
+      <Flex direction={"row"} gap={6}>
+        <Box
+          position={"relative"}
+          flexShrink={0}
+          w={"300px"}
+          h={"auto"}
+          border={"1px solid #e0e0e0"}
         >
-          {sorts.map((sort, index) => {
-            return (
-              <Button
-                key={index}
-                border={"1px solid #e0e0e0"}
-                p={2}
-                borderRadius={"24px"}
-                _hover={{ borderColor: "orange" }}
-                fontSize={"14px"}
-                bgColor={
-                  searchParams.get("sortQuery") === sort.value
-                    ? "orange"
-                    : "white"
-                }
-                color={
-                  searchParams.get("sortQuery") === sort.value
-                    ? "white"
-                    : "black"
-                }
-                onClick={() => {
-                  router.push(
-                    pathname +
-                      "?" +
-                      setQueryString(sort.queryString, sort.value)
-                  );
-                }}
-              >
-                {sort.name}
-              </Button>
-            );
-          })}
-        </Flex>
-        <Flex
-          direction={"row"}
-          maxH={"80px"}
-          justifyContent={"flex-start"}
-          alignItems={"center"}
-          gap={3}
-        >
-          {filters.map((filter) =>
-            searchParams.getAll(filter.queryString).map((value, index) => {
+          <Accordion
+            position={"sticky"}
+            h={"100vh"}
+            top={"0px"}
+            overflowY={"auto"}
+            allowMultiple
+            defaultIndex={[...filters.map((element, index) => index)]}
+          >
+            {filters.map((filter, index) => {
+              return (
+                <AccordionItem key={index}>
+                  <AccordionButton>
+                    <Box as="span" flex="1" textAlign="left">
+                      {filter.name}
+                    </Box>
+                    <AccordionIcon />
+                  </AccordionButton>
+                  <AccordionPanel pb={4}>
+                    <Flex direction={"column"} gap={3}>
+                      {filter.type === "checkbox" &&
+                        filter.values.map((value, index) => {
+                          return (
+                            <Checkbox
+                              key={index}
+                              colorScheme="orange"
+                              fontWeight={600}
+                              isChecked={searchParams
+                                .getAll(filter.queryString)
+                                .includes(value)}
+                              onChange={(e) => {
+                                e.target.checked
+                                  ? router.push(
+                                      pathname +
+                                        "?" +
+                                        createQueryString(
+                                          filter.queryString,
+                                          value
+                                        )
+                                    )
+                                  : router.push(
+                                      pathname +
+                                        "?" +
+                                        deleteQueryString(
+                                          filter.queryString,
+                                          value
+                                        )
+                                    );
+                              }}
+                            >
+                              {value}
+                            </Checkbox>
+                          );
+                        })}
+                    </Flex>
+                  </AccordionPanel>
+                </AccordionItem>
+              );
+            })}
+          </Accordion>
+        </Box>
+
+        <Flex direction={"column"} w={"100%"} gap={3}>
+          <Flex
+            direction={"row"}
+            h={"80px"}
+            justifyContent={"flex-start"}
+            alignItems={"center"}
+            gap={3}
+          >
+            {sorts.map((sort, index) => {
               return (
                 <Button
-                  leftIcon={<Icon as={CloseIcon} w={2} h={2} />}
-                  bgColor={"orange"}
+                  key={index}
+                  border={"1px solid #e0e0e0"}
+                  p={2}
+                  borderRadius={"24px"}
+                  _hover={{ borderColor: "orange" }}
+                  fontSize={"14px"}
+                  bgColor={
+                    searchParams.get("sortQuery") === sort.value
+                      ? "orange"
+                      : "white"
+                  }
+                  color={
+                    searchParams.get("sortQuery") === sort.value
+                      ? "white"
+                      : "black"
+                  }
                   onClick={() => {
                     router.push(
                       pathname +
                         "?" +
-                        deleteQueryString(filter.queryString, value)
+                        setQueryString(sort.queryString, sort.value)
                     );
                   }}
                 >
-                  {value}
+                  {sort.name}
                 </Button>
               );
-            })
-          )}
-        </Flex>
-        <Flex
-          direction={"row"}
-          alignItems={"center"}
-          justifyContent={"flex-start"}
-          flexWrap={"wrap"}
-          gap={8}
-        >
-          {products
-            .filter((item) => item.category === params.name)
-            .map((item, key) => {
-              return <Product item={item} key={key} />;
             })}
+          </Flex>
+          <Flex
+            direction={"row"}
+            maxH={"80px"}
+            justifyContent={"flex-start"}
+            alignItems={"center"}
+            gap={3}
+          >
+            {filters.map((filter) =>
+              searchParams.getAll(filter.queryString).map((value, index) => {
+                return (
+                  <Button
+                    key={index}
+                    leftIcon={<Icon as={CloseIcon} w={2} h={2} />}
+                    bgColor={"orange"}
+                    onClick={() => {
+                      router.push(
+                        pathname +
+                          "?" +
+                          deleteQueryString(filter.queryString, value)
+                      );
+                    }}
+                  >
+                    {value}
+                  </Button>
+                );
+              })
+            )}
+          </Flex>
+          <Flex
+            direction={"row"}
+            alignItems={"center"}
+            justifyContent={"flex-start"}
+            flexWrap={"wrap"}
+            gap={8}
+          >
+            {products
+              .filter((item) => item.category === params.name)
+              .map((item, key) => {
+                return <Product item={item} key={key} />;
+              })}
+          </Flex>
         </Flex>
       </Flex>
-    </Flex>
+    </Box>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,6 @@ export default function RootLayout({
             <Header />
             <Navbar />
             {children}
-            <Footer />
           </main>
         </Providers>
       </body>
